b2cce49183ec2f23fdb1722562d8cbf25836017a
Frank W <frankwu26@gmail.com>
2020-01-18 14:43:22 -0500

    add fabric

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +20,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <PrimaryButton>I am a button.</PrimaryButton>
     </div>
   );
 }
